5d9bdae89c70acdafd4f6844ad4b225688a7fcdc
tomad6909 <tomad6909@wrdsb.ca>
2020-10-13 16:17:49 +0000

    finished A7Q2

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
 /**
- *
- * @author 
+ * This program will take an integer as a parameter and will print out every number that will divide the parameter evenly
+ * @author David Toma
  */
 public class Main {
+
+  // create a method 
+  public static int factors(int factor){
+    // create a for statement 
+    for(int i = 1; i <= factor; i++){
+      // nested if statement to find the remainder
+      if(factor %i == 0){
+        System.out.println(i);
+      }
+    }
+    // return the users number
+    return factor;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // input the number that the user wants to divide
+    factors(10);
+  
     
   }
 }
